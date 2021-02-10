68a68e7e34c2ef5e687d6bdbf916f6324fff17cb
Fabiano Tomas <fabianotomasnovais@gmail.com>
2021-02-10 01:19:59 -0300

    Banco de dados H2 e criação automatica da base de dados

--- src/main/java/com/escritaacademica/domain/Categoria.java
+++ src/main/java/com/escritaacademica/domain/Categoria.java
@@ -2,8 +2,16 @@ package com.escritaacademica.domain;
 
 import java.io.Serializable;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+@Entity
 public class Categoria implements Serializable{
 	private static final long serialVersionUID = 1L;
+	@Id
+	@GeneratedValue(strategy=GenerationType.IDENTITY) //Essa estrategia funciona com maioria do banco de dados, verificar se funciona com o postgresql
 	private Integer id;
 	private String nome;
 	
