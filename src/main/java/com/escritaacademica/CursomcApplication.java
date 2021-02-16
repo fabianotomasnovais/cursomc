d92b0c03a540b8900a17ed5c80a015bdc5b29016
Fabiano Tomas <fabianotomasnovais@gmail.com>
2021-02-16 09:20:55 -0300

    Creating instance operation

--- src/main/java/com/escritaacademica/CursomcApplication.java
+++ src/main/java/com/escritaacademica/CursomcApplication.java
@@ -1,13 +1,32 @@
 package com.escritaacademica;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import com.escritaacademica.domain.Categoria;
+import com.escritaacademica.repositories.CategoriaRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner {
+
+	@Autowired
+	private CategoriaRepository categoriaRepository;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
 
+	// Execute an action on start
+	@Override
+	public void run(String... args) throws Exception {
+		// TODO Auto-generated method stub
+		Categoria cat1 = new Categoria(null, "Computers");
+		Categoria cat2 = new Categoria(null, "Office");
+
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
+	}
 }
