9e8dfe114dd50eb4938cf1bf5a406230fb456486
Fabiano Tomas <fabianotomasnovais@gmail.com>
2021-02-23 20:39:35 -0300

    Endpoint adjustments /categoria/{id}

--- src/main/java/com/escritaacademica/CursomcApplication.java
+++ src/main/java/com/escritaacademica/CursomcApplication.java
@@ -28,14 +28,17 @@ public class CursomcApplication implements CommandLineRunner {
 	@Override
 	public void run(String... args) throws Exception {
 		// TODO Auto-generated method stub
+		
+		//Create the categories
 		Categoria cat1 = new Categoria(null, "Computers");
 		Categoria cat2 = new Categoria(null, "Office");
 		
+		//Create the products
 		Produto p1 = new Produto(null, "Computer", 2000.00);
 		Produto p2 = new Produto(null, "Printer", 800.00);
 		Produto p3 = new Produto(null, "Mouse", 80.00);
 		
-		
+		//Make the relationships 
 		cat1.getProdutos().addAll(Arrays.asList(p1,p2,p3));
 		cat2.getProdutos().addAll(Arrays.asList(p2));
 		
--- src/main/java/com/escritaacademica/services/CategoriaService.java
+++ src/main/java/com/escritaacademica/services/CategoriaService.java
@@ -7,16 +7,20 @@ import org.springframework.stereotype.Service;
 
 import com.escritaacademica.domain.Categoria;
 import com.escritaacademica.repositories.CategoriaRepository;
+import com.escritaacademica.services.exceptions.ObjectNotFoundException;
 
 @Service
 public class CategoriaService {
-	
+
 	@Autowired
 	private CategoriaRepository repo;
-	
+
 	public Categoria buscar(Integer id) {
+		// Optional<Categoria> obj = repo.findById(id);
+		// return obj.orElse(null);
 		Optional<Categoria> obj = repo.findById(id);
-		return obj.orElse(null);
+		return obj.orElseThrow(() -> new ObjectNotFoundException(
+				"Objeto não encontrado! Id: " + id + ", Tipo: " + Categoria.class.getName()));
 	}
 
 }
