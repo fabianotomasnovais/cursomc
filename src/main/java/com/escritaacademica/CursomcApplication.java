b6390db5fdd60603c34dc1acce1764cf3801ebe5
Fabiano Tomas <fabianotomasnovais@gmail.com>
2021-03-10 23:08:28 -0300

    Order, PaymentState, Payment

--- src/main/java/com/escritaacademica/CursomcApplication.java
+++ src/main/java/com/escritaacademica/CursomcApplication.java
@@ -1,5 +1,6 @@
 package com.escritaacademica;
 
+import java.text.SimpleDateFormat;
 import java.util.Arrays;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -12,13 +13,20 @@ import com.escritaacademica.domain.Cidade;
 import com.escritaacademica.domain.Cliente;
 import com.escritaacademica.domain.Endereco;
 import com.escritaacademica.domain.Estado;
+import com.escritaacademica.domain.Pagamento;
+import com.escritaacademica.domain.PagamentoComBoleto;
+import com.escritaacademica.domain.PagamentoComCartao;
+import com.escritaacademica.domain.Pedido;
 import com.escritaacademica.domain.Produto;
+import com.escritaacademica.domain.enums.EstadoPagamento;
 import com.escritaacademica.domain.enums.TipoCliente;
 import com.escritaacademica.repositories.CategoriaRepository;
 import com.escritaacademica.repositories.CidadeRepository;
 import com.escritaacademica.repositories.ClienteRepository;
 import com.escritaacademica.repositories.EnderecoRepository;
 import com.escritaacademica.repositories.EstadoRepository;
+import com.escritaacademica.repositories.PagamentoRepository;
+import com.escritaacademica.repositories.PedidoRepository;
 import com.escritaacademica.repositories.ProdutoRepository;
 
 @SpringBootApplication
@@ -36,6 +44,10 @@ public class CursomcApplication implements CommandLineRunner {
 	private ClienteRepository clienteRepository;
 	@Autowired
 	private EnderecoRepository enderecoRepository;	
+	@Autowired
+	private PedidoRepository pedidoRepository;
+	@Autowired
+	private PagamentoRepository pagamentoRepository;	
 	
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
@@ -66,9 +78,11 @@ public class CursomcApplication implements CommandLineRunner {
 		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
 		produtoRepository.saveAll(Arrays.asList(p1,p2,p3));
 		
+		//Create states
 		Estado est1 = new Estado(null, "Minas Gerais");
 		Estado est2 = new Estado(null,"São Paulo");
 		
+		//Create cities
 		Cidade c1 = new Cidade(null,"Ouro Preto",est1);
 		Cidade c2 = new Cidade(null,"Cachoeira do Campo 2",est1);
 		Cidade c3 = new Cidade(null,"Campinas",est2);
@@ -79,9 +93,11 @@ public class CursomcApplication implements CommandLineRunner {
 		estadoRepository.saveAll(Arrays.asList(est1,est2));
 		cidadeRepository.saveAll(Arrays.asList(c1,c2,c3));
 		
+		//Create clients
 		Cliente cli1 = new Cliente(null,"Aline Silva","linesialho@gmail.com","04508057085",TipoCliente.PESSOAFISICA);
 		cli1.getTelefones().addAll(Arrays.asList("35515555","975277682"));
 		
+		//Create address
 		Endereco e1 = new Endereco(null, "Rua das Flores", "20", "Apt 304", "Centro", "35410000", cli1, c1);
 		Endereco e2 = new Endereco(null, "Rua Rezende", "45", "Casa", "Antônio Dias", "35400000", cli1, c2);
 		
@@ -89,6 +105,24 @@ public class CursomcApplication implements CommandLineRunner {
 		
 		clienteRepository.saveAll(Arrays.asList(cli1));
 		enderecoRepository.saveAll(Arrays.asList(e1,e2));
+		
+		//Create orders
+		SimpleDateFormat sdf = new SimpleDateFormat("dd/MM/yyyy HH:mm");
+		Pedido ped1 = new Pedido(null, sdf.parse("30/09/2017 10:32"), cli1, e1);
+		Pedido ped2 = new Pedido(null, sdf.parse("10/10/2017 19:35"), cli1, e2);
+		
+		//Create payment
+		Pagamento pagto1 = new PagamentoComCartao(null, EstadoPagamento.QUITADO, ped1, 6);
+		ped1.setPagamento(pagto1);
+		
+		Pagamento pagto2 = new PagamentoComBoleto(null, EstadoPagamento.PENDENTE, ped2,sdf.parse("20/10/2017 00:00"), null);
+		ped2.setPagamento(pagto2);
+		
+		cli1.getPedidos().addAll(Arrays.asList(ped1,ped2));
+		
+		pedidoRepository.saveAll(Arrays.asList(ped1,ped2)); //Salvar pedido antes de pagamento
+		pagamentoRepository.saveAll(Arrays.asList(pagto1,pagto2));
+		
 		
 	}
 }
---
+++ src/main/java/com/escritaacademica/repositories/PedidoRepository.java
@@ -0,0 +1,12 @@
+package com.escritaacademica.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.escritaacademica.domain.Pedido;
+
+//Realiza operações de acesso a dados no banco de dados (buscar, deletar, adicionar,editar)
+@Repository
+public interface PedidoRepository extends JpaRepository<Pedido, Integer> {
+
+}
